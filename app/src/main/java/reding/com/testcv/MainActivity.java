f9c5d4ec58d4839eecef4b9133cf4ade1f4985ba
guojianpeng <guojianpeng@meituan.com>
2017-08-11 15:32:10 +0800

    test

--- app/src/main/java/reding/com/testcv/MainActivity.java
+++ app/src/main/java/reding/com/testcv/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
@@ -19,49 +17,43 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
+    static {
+        if (!OpenCVLoader.initDebug()) {
 
-                }
-                break;
-                default: {
-                    super.onManagerConnected(status);
-                }
-                break;
-            }
         }
-    };
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
         setContentView(R.layout.activity_main);
         ImageView src = (ImageView) findViewById(R.id.image_src);
         ImageView des = (ImageView) findViewById(R.id.image_des);
         try {
             Bitmap srcBm = BitmapFactory.decodeStream(getAssets().open("1.png"));
+
             src.setImageBitmap(srcBm);
+
             Mat srcMat = Mat.zeros(src.getWidth(), src.getHeight(), CvType.CV_8UC1);
+
             Utils.bitmapToMat(srcBm, srcMat);
 
             Mat grayMat = Mat.zeros(srcMat.size(), CvType.CV_8UC1);
 
-
             Imgproc.cvtColor(srcMat, grayMat, Imgproc.COLOR_BGR2GRAY);
 
             Mat edge = Mat.zeros(srcMat.size(), CvType.CV_8UC1);
-            Imgproc.blur(grayMat, edge, new Size(3, 3));
 
-            Imgproc.Canny(edge, edge, 3, 9);
+            Imgproc.blur(grayMat, edge, new Size(9, 9));
 
+            Imgproc.Canny(edge, edge, 9, 9);
 
-            Utils.matToBitmap(edge, srcBm);
+            Bitmap bitmap = BitmapFactory.decodeStream(getAssets().open("1.png"));
 
-            des.setImageBitmap(srcBm);
+            Utils.matToBitmap(edge, bitmap);
+
+            des.setImageBitmap(bitmap);
 
         } catch (IOException e) {
             e.printStackTrace();
